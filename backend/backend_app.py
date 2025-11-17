7079f2e3bdc84e13b7932804bfa96aead5402109
Emilio Quezada <equezadanavarro@gmail.com>
2025-11-17 22:57:39 +0100

    'Add' endpoint added

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,6 +13,35 @@ POSTS = [
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+
+    # Getting form data
+    data = request.get_json()
+
+    # Validation all fields are mandatory
+    if not data or 'title' not in data or 'content' not in data:
+        return jsonify({"error": "Missing title or content"}), 400
+
+    # Creating a new ID
+    if POSTS:
+        max_id = max(post['id'] for post in POSTS)
+        new_id = max_id + 1
+    else:
+        new_id = 1
+
+    # Creating a new post dict
+    new_post = {
+        'id': new_id,
+        'title': data['title'],
+        'content': data['content']
+    }
+
+    # Add the new post to the POST list
+    POSTS.append(new_post)
+
+    return jsonify(new_post), 201
 
 
 if __name__ == '__main__':
